b21d8cbf030a1b1f7685bffec348e7fda403c3b9
ANKIT PRIYADARSHI <72643034+ankitpriyadarshii@users.noreply.github.com>
2023-03-11 20:38:08 +0530

    Update Day49-1-CheckCircularLL-1.cpp

--- Day49-1-CheckCircularLL-1.cpp
+++ Day49-1-CheckCircularLL-1.cpp
@@ -1,6 +1,7 @@
-// Finding middle element in a linked list
-/*Given a singly linked list of N nodes.
-The task is to find the middle of the linked list.*/
+// Check If Circular Linked List
+/*Given head, the head of a singly linked list, find if the linked list is circular or not. 
+A linked list is called circular if it not NULL terminated and all nodes are connected in the form of a cycle. 
+An empty linked list is considered as circular.*/
 
 #include <iostream>
 #include <algorithm>
