b579356ff4cbe9654ac92c1a5c07277f0bfa6e69
ANKIT PRIYADARSHI <72643034+ankitpriyadarshii@users.noreply.github.com>
2023-03-29 21:12:41 +0530

    Add files via upload

---
+++ Day66-1-FindLCAinBST.cpp
@@ -0,0 +1,120 @@
+// Lowest Common Ancestor in a BST
+/*Given a Binary Search Tree (with all values unique) and two node values. 
+Find the Lowest Common Ancestors of the two nodes in the BST. */
+
+
+//{ Driver Code Starts
+#include <iostream>
+#include <stdlib.h>
+#include <sstream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+Node* LCA(Node* root, int n1, int n2);
+
+int main() {
+
+    string s;
+    getline(cin, s);
+    Node* root = buildTree(s);
+    
+    int l, h;
+    cin >> l >> h;
+    cout << LCA(root, l, h)->data;
+
+    return 0;
+}
+// } Driver Code Ends
+
+
+
+
+//Function to find the lowest common ancestor in a BST. 
+Node* LCA(Node* root, int n1, int n2)
+{
+    //Your code here
+    if (root == NULL) return NULL;
+
+    if (n1 < root->data && n2 < root->data)
+        return LCA(root->left, n1, n2);
+
+    if (n1 > root->data && n2 > root->data)
+        return LCA(root->right, n1, n2);
+
+    return root;
+}
